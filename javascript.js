40e01b330f3d2e0709e18a8e7e9df841b23ff7d1
Lorealius <lukastijunaitis27@gmail.com>
2024-07-16 15:14:48 +0100

    Modified playRound Function + Added Game Score code
    updated the playRound function to include human + computer inputes to be calculated within the scope of the function.Also added game score mechanics by adding a for loop and count tracker for the game.

--- javascript.js
+++ javascript.js
@@ -4,59 +4,83 @@ function getComputerChoice() {
     return(array[(Math.floor(Math.random() * array.length))]);
 }
 
-const computerChoice = getComputerChoice();
-const humanChoice = prompt('type your pick! Rock, Paper or Scissors?').toLowerCase();
 
-console.log('Computer has picked ' + computerChoice);
-
-function getHumanChoice() {
+function getHumanChoice(humanChoice) {
     if (humanChoice === 'rock') {
-        return ('you have picked rock');
+        console.log('you have picked: rock');
     } else if (humanChoice === 'paper') {
-        return ('you have picked paper');
+        console.log('you have picked paper');
     } else if (humanChoice === 'scissors') {
-        return ('you have picked scissors');
+        console.log('you have picked scissors');
     } else {
         console.log('Please choose from Rock, Paper or Scissors')
     }
 }
 
-const humanReturn = getHumanChoice();
-console.log(humanReturn);
-
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound (humanChoice, computerChoice) {
+function playRound () {
+    let humanChoice = prompt('type your pick! Rock, Paper or Scissors?').toLowerCase();
+    getHumanChoice(humanChoice);
+    let computerChoice = getComputerChoice();
+    console.log('Computer has picked: ' + computerChoice);
     if (humanChoice === computerChoice) {
-        return('It is a draw');
+        console.log('It is a draw');
     } else if (humanChoice === 'rock' && computerChoice === 'paper') {
         computerScore += 1;
-        return('you loose! paper beats rock');
+        console.log('you loose! paper beats rock');
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         humanScore += 1;
-        return('you win! rock beats scissors');
+        console.log('you win! rock beats scissors');
     } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
         computerScore += 1;
-        return('you loose! scissors beats paper');
+        console.log('you loose! scissors beats paper');
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
         humanScore += 1;
-        return('you win! paper beats rock');
+        console.log('you win! paper beats rock');
     } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
         computerScore += 1;
-        return('you loose! rock beats scissors');
+        console.log('you loose! rock beats scissors');
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         humanScore += 1;
-        return('you win! scissors beats paper');
+        console.log('you win! scissors beats paper');
     }
 }
 
  function gameScore () {
-    console.log('Your Current Score: ' + humanScore);
-    console.log('Computers Current Score: ' + computerScore);
+    console.log('Your Current Score is : ' + humanScore);
+    console.log('Computers Current Score is : ' + computerScore);
  }
 
 
- console.log(playRound(humanChoice, computerChoice));
- gameScore();
+ function declareWinner () {
+    if (humanScore > computerScore) {
+        console.log('You have Won!!');
+    } else if (humanScore < computerScore) {
+        console.log('You have Lost!')
+    } else {
+        console.log('It is a draw!')
+    }
+ }
 
+ function playGame () {
+    let currentCount = 0
+    let maxRounds = 5
+
+    function play() {
+        if (currentCount < maxRounds) {
+            playRound();
+            gameScore();
+            currentCount++;
+        } 
+    }
+
+    for (let i = 0; i < maxRounds; i++) {
+        play();
+    }
+    declareWinner();
+ }
+
+
+ playGame();
